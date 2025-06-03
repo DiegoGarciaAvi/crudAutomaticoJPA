10f04e6c6f6988baf94b4454f8b71153a64adade
DiegoGarciaAvi <diego_201723075@test.edu.mx>
2025-06-03 09:24:14 -0600

    Formated query

--- src/main/java/com/crud_automatico/Persistence/Repository/TableRepository.java
+++ src/main/java/com/crud_automatico/Persistence/Repository/TableRepository.java
@@ -5,15 +5,13 @@ import com.crud_automatico.Persistence.Projection.TableProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface TableRepository extends JpaRepository<TableEntity, Long> {
 
     @Query(value = "SELECT TABLE_NAME as tableName \n" +
-                    "FROM information_schema.tables\n" +
+                    "FROM information_schema.tables \n " +
                     "WHERE table_schema='public' \n" +
                     "AND table_type='BASE TABLE';", nativeQuery = true)
     List<TableProjection> findAllByTablesNamesPostgres();
